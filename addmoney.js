6aea5fc346df24fcdaeeffd3dd647a6e1f6ae44c
hassankhsalar <khsalarhassan@gmail.com>
2024-09-24 15:18:01 +0600

    add fund

---
+++ addmoney.js
@@ -0,0 +1,24 @@
+document.getElementById('card-1-donate').addEventListener('click', function(){
+   
+    const fundC1 = document.getElementById('card-1-fund').innerText;
+    const fundC1Number = parseInt(fundC1);
+   
+    const addMoneyC1 = document.getElementById('card-1-input').value;
+    const addMoneyC1Number = parseInt(addMoneyC1);
+
+    const userBalance = document.getElementById('user-balance').innerText;
+    const userBalanceNumber = parseInt(userBalance);
+    //console.log(addMoneyC1, userBalance);
+
+    if(userBalanceNumber => addMoneyC1Number){
+
+       const currentFund = fundC1Number + addMoneyC1Number;
+       const currentBalance = userBalanceNumber - addMoneyC1Number;
+
+       document.getElementById('card-1-fund').innerText = currentFund;
+       document.getElementById('user-balance').innerText = currentBalance;
+    }
+    else{
+        alert('Insufficient Balance!')
+    }
+})
---
+++ index.js
@@ -0,0 +1,4 @@
+const calculateDonation = document.getElementById('card-1-donate');
+calculateDonation.addEventListener('click',function(){
+    console.log('kaj kortese');
+})
